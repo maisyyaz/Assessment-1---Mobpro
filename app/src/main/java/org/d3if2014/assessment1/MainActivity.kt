8d37f04679ddcca858e37b3ce723c0c0066a44ea
yazid138 <ma39isy@gmail.com>
2022-03-28 15:55:54 +0700

    membuat layout dan memproses data inputan

--- app/src/main/java/org/d3if2014/assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if2014/assessment1/MainActivity.kt
@@ -2,10 +2,44 @@ package org.d3if2014.assessment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import org.d3if2014.assessment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener { hitung() }
+    }
+
+    private fun hitung() {
+        val angka1 = binding.angka1.text.toString()
+        if (TextUtils.isEmpty(angka1)) {
+            Toast.makeText(this, R.string.angka1_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val angka2 = binding.angka2.text.toString()
+        if (TextUtils.isEmpty(angka2)) {
+            Toast.makeText(this, R.string.angka2_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val operator = binding.selectOperasi.selectedItem.toString()
+
+        val hasil = when (operator) {
+            "+" -> angka1.toDouble() + angka2.toDouble()
+            "-" -> angka1.toDouble() - angka2.toDouble()
+            "*" -> angka1.toDouble() * angka2.toDouble()
+            "/" -> if (angka2.toDouble() == 0.0) 0.0 else angka1.toDouble() / angka2.toDouble()
+            else -> 0
+        }
+
+        binding.tvHasil.text = getString(R.string.hasil, hasil.toDouble())
     }
 }
